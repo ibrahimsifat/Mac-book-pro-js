439db3305713ddf359a973de470552e781a28559
ibrahimsifat <ibsifat24@gmail.com>
2021-08-20 21:09:36 +0300

    html

---
+++ app.js
@@ -0,0 +1,17 @@
+   // all selection
+   // memory 
+   let memory8 = document.getElementById('8gbMemory')
+   let memory16 = document.getElementById('16gbMemory')
+       // storage
+   let storage256 = document.getElementById('256gbStorage')
+   let storage512 = document.getElementById('512gbStorage')
+   let storage1tb = document.getElementById('1tbStorage')
+       // prices innner text 
+   let bestPrice = document.getElementById('bestPrice')
+   let extraMemory = document.getElementById('extraMemory')
+   let extraStorage = document.getElementById('extraStorage')
+   let delivaryCharge = document.getElementById('delivaryCharge')
+   let totalPrice = document.getElementById('totalPrice')
+       // promo input and button
+   let promoInput = document.getElementById('promoInput')
+   let PromoApply = document.getElementById('PromoApply')
